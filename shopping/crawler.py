8546c880e1cda1247ec741ed1ffbe31d3ac6dca1
{LEESM} <LEESM@LEESMui-MacBook-Pro.local>
2016-05-10 01:02:34 +0900

    crawler

--- shopping/crawler.py
+++ shopping/crawler.py
@@ -4,37 +4,46 @@ import django
 django.setup()
 from selenium import webdriver
 import time
-import datetime
-import random
-import xlwt
+#from ingredient.models import Ingredient
 
-def makeurl(page):
-	first_url = 'http://m.ssg.com/search.ssg?target=mobile&query=mcm&viewType=lst&page='
-	url = first_url + str(page)
-	return(url)
+#46732 시작
+#57103 끝
+def old_crawler():
+	print('크롤링 시작')
+	driver = webdriver.Firefox()
+	driver.get("https://www.kcia.or.kr/cid/Document/020.Ingredient_shis/INGREDIENT_SHIS_10L.asp")
+	for no in range(46732,57103):
+		while True:
+			try:
+				driver.execute_script("fView('{no}')".format(no=no))
+				ko_name = driver.find_element_by_css_selector('table:nth-child(3) tr:nth-child(1) td:nth-child(2)')
+				if ko_name.text=='':
+					print('빈 번호')
+					break
+				en_name = driver.find_element_by_css_selector('table:nth-child(3) tr:nth-child(2) td:nth-child(2)')
+				purpose = driver.find_element_by_css_selector('table:nth-child(3) tr:nth-child(8) td:nth-child(2)')
+				#q=Ingredient(ko_name=ko_name.text, en_name=en_name.text, purpose=purpose.text)
+				#q.save()
+				print(str(no)+ko_name.text+en_name.text+purpose.text)
+				driver.get("https://www.kcia.or.kr/cid/Document/020.Ingredient_shis/INGREDIENT_SHIS_10L.asp")
+				break
+			except:
+				print('에러발생')
+				time.sleep(5)
 
-def makedetail(item_id):
-	first_url = 'http://www.ssg.com/item/itemView.ssg?itemId='
-	url = first_url+str(item_id)
-	return(url)
 
-def crawler():
-	driver=webdriver.Firefox()
-	for page in range(1,22):
-		driver.get(makeurl(page))
-		item_list =driver.find_elements_by_name('attnTgtIdnfNo1')
-		for item in item_list:
-			item_id = item.get_attribute("value")
-			driver.get(makedetail(item_id))
-			item_name = driver.find_element_by_class_name("item_title")
-			item_price = driver.find_element_by_class_name("ssg_price price")
-			item_content = driver.find_elemnt_by_id("descContents")
-			print(item_name)
-			print(item_price)
-			print(item_content)
-	driver.quit()
+#503page, 16.5.9.
+def new_crawler():
+	print('크롤링 시작')
+	driver = webdriver.Firefox()
+	driver.get("https://www.kcia.or.kr/cid/Document/020.Ingredient_shis/INGREDIENT_SHIS_10L.asp")
+	for i in range(503,0,-1):
+		while True:
+			try:
+				driver.execute_script("fGoPage('{i}')".format(i=i))
 
-def main():
-	crawler()
+			except:
+				print('에러')
+				time.sleep(3)
 
-main()
+old_crawler()
---
+++ shopping/ingredient/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Ingredient(models.Model):
+	kcia_no = IntegerField(unique=True)
+	ko_name = models.CharField(max_length=2000, null=True, blank=True)
+	en_name = models.CharField(max_length=2000, null=True, blank=True)
+	purpose = models.TextField(null=True, blank=True)
+	ewg_high_grade = models.IntegerField(default=-1)
+	ewg_low_grade = models.IntegerField(default=-1)
+	caution20 = models.IntegerField(default=0)
+	pub_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.ko_name
