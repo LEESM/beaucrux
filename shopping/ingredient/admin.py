7cfab1d0ecf664fbf73255d54c2d6801f383f63c
{LEESM} <LEESM@LEESMui-MacBook-Pro.local>
2016-05-10 16:48:10 +0900

    ingredient

--- shopping/ingredient/admin.py
+++ shopping/ingredient/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from ingredient.models import ingredient
+from ingredient.models import Ingredient
 from django_summernote.admin import SummernoteModelAdmin
 
-class ingredientAdmin(SummernoteModelAdmin):
+class IngredientAdmin(SummernoteModelAdmin):
 	list_display = ['kcia_no','ko_name','en_name','purpose','ewg_high_grade','ewg_low_grade','caution20','pub_date',]
 
-admin.site.register(ingredient, ingredientAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 
