5e0e6061622d6481e06cfed110b6ca7be5cbd687
{LEESM} <LEESM@LEESMui-MacBook-Pro.local>
2016-06-04 13:42:32 +0900

    admin modified

--- shopping/ingredient/admin.py
+++ shopping/ingredient/admin.py
@@ -3,6 +3,8 @@ from ingredient.models import Ingredient
 from django_summernote.admin import SummernoteModelAdmin
 
 class IngredientAdmin(SummernoteModelAdmin):
-	list_display = ['kcia_no','ko_name','en_name','purpose','ewg_high_grade','ewg_low_grade','caution20','pub_date',]
+	list_display = ['id','kcia_no','ko_name','en_name','purpose','ewg_high_grade','ewg_low_grade','caution20','pub_date',]
+	search_fields = ['ko_name']
+
 admin.site.register(Ingredient, IngredientAdmin)
 
