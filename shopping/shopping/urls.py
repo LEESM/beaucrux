12df1c8f1e6144c8227ae5110f7c6826b581d877
LEESM <fast0522@gmail.com>
2016-07-21 18:04:49 +0900

    open really

--- shopping/shopping/urls.py
+++ shopping/shopping/urls.py
@@ -20,8 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^index/', index, name='index'),
-    url(r'^$', landing, name='landing'),
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^aboutus/', include('aboutus.urls')),
     url(r'^accounts/', include('accounts.urls')),
